31111b34890fe9cad3ab044888c6d1110f44bf40
Jacques Berger <jacques.berger@code3.ca>
2015-05-08 01:03:15 -0400

    Setup initial pour le premier cours

---
+++ Cours1/setup.sql
@@ -0,0 +1,15 @@
+drop table albums;
+
+create table albums (
+  artiste varchar2(100),
+  titre varchar2(200),
+  annee number,
+  commentaire varchar2(300),
+  note number
+);
+
+insert into albums values("Michael Jackson", "Thriller", 1982, "Un classique", 5);
+insert into albums values("Michael Jackson", "Bad", 1987, "5 ans pour un deuxième album?!!?", 5);
+insert into albums values("Michael Jackson", "Dangerous", 1991, "Moins excellent, mais bon quand même", 4);
+insert into albums values("Michael Bublé", "Call Me Irresponsible", 2007, "", 4);
+insert into albums values("Michael Bublé", "Crazy Love", 2009, "Rien à dire", 3);
