dcecfada9c86cff852194a7dc9965742599d1d12
Jacques Berger <jacques.berger@code3.ca>
2015-05-08 01:11:58 -0400

    Retirer les caractères non-ASCII

--- Cours1/setup.sql
+++ Cours1/setup.sql
@@ -9,7 +9,7 @@ create table albums (
 );
 
 insert into albums values('Michael Jackson', 'Thriller', 1982, 'Un classique', 5);
-insert into albums values('Michael Jackson', 'Bad', 1987, '5 ans pour un deuxième album?!!?', 5);
-insert into albums values('Michael Jackson', 'Dangerous', 1991, 'Moins excellent, mais bon quand même', 4);
-insert into albums values('Michael Bublé', 'Call Me Irresponsible', 2007, '', 4);
-insert into albums values('Michael Bublé', 'Crazy Love', 2009, 'Rien à dire', 3);
+insert into albums values('Michael Jackson', 'Bad', 1987, '5 ans pour un deuxieme album?!!?', 5);
+insert into albums values('Michael Jackson', 'Dangerous', 1991, 'Moins excellent, mais bon quand meme', 4);
+insert into albums values('Michael Buble', 'Call Me Irresponsible', 2007, '', 4);
+insert into albums values('Michael Buble', 'Crazy Love', 2009, 'Rien a dire', 3);
