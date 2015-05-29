8e978f5012629ee846c09cc13b617f896f0fb800
Jacques Berger <jacques.berger@code3.ca>
2015-05-29 01:39:36 -0400

    Correctif et diversité

--- Cours4/script.sql
+++ Cours4/script.sql
@@ -4,9 +4,8 @@ create table Etudiant (
   id number(38),
   nom varchar2(40) not null,
   prenom varchar2(40) not null,
-  sexe number(1) not null,
+  sexe number(1) not null check(sexe in (0, 1, 2, 9)),
   age number(3) not null,
-  constraint pk_etudiant primary key (id)
-  check (sexe in (0, 1, 2, 9)),
+  constraint pk_etudiant primary key (id),
   check (age >= 18 and age < 125)
 );
