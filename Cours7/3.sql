d01dca776aad6a87c5fb8fa3fc3fa2ed2a1a97d9
Jacques Berger <jacques.berger@code3.ca>
2015-06-26 02:09:36 -0400

    3ème exemple

---
+++ Cours7/3.sql
@@ -0,0 +1,20 @@
+show errors
+
+create or replace procedure afficherSiAucunAlbum is
+  nombreAlbums NUMBER;
+begin
+  select count(*)
+  into nombreAlbums
+  from album;
+
+  if nombreAlbums = 0 then
+    dbms_output.put_line('Aucun album dans la table.');
+  end if;
+  
+  if nombreAlbums = 16 then
+    dbms_output.put_line('Chargement initial.');
+  else
+    dbms_output.put_line('Alteration de la table.');
+  end if;
+end;
+/
