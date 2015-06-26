6b603e57c25908fd35005da88c7e8c9715dddd41
Jacques Berger <jacques.berger@code3.ca>
2015-06-26 02:20:15 -0400

    Exemple insertion

---
+++ Cours7/4.sql
@@ -0,0 +1,27 @@
+show errors
+
+create or replace procedure ajouterAlbum 
+  (titreAlbum album.titre%type,
+   anneeAlbum album.annee%type,
+   maisonDisque maison_disque.nom%type,
+   artiste artiste.nom%type) is
+  idArtiste artiste.id%type;
+  idMaisonDisque maison_disque.id%type;
+begin
+  select id
+  into idMaisonDisque
+  from maison_disque
+  where nom = maisonDisque;
+
+  select id
+  into idArtiste
+  from artiste
+  where nom = artiste;
+
+  insert into album values (album_seq.nextval, titreAlbum, anneeAlbum, idArtiste, idMaisonDisque);
+  dbms_output.put_line('Enregistre');
+exception
+  when no_data_found then
+    dbms_output.put_line('Erreur de reference');
+end;
+/
