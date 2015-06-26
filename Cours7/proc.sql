908b7e99d638967ebb1df7dae98d5c046521ff2f
Jacques Berger <jacques.berger@code3.ca>
2015-06-26 00:49:57 -0400

    Ajouter le mot-clé begin

--- Cours7/proc.sql
+++ Cours7/proc.sql
@@ -2,6 +2,8 @@ drop procedure afficherParametre;
 
 create procedure afficherParametre
   (param NUMBER) is
+  -- Declarations
+begin
   dbms_output.put_line('Voici le parametre : ' || to_char(param));
 end afficherParametre;
 /
