dcff847051039bf3ed66c302d1e95230d6f66579
Jacques Berger <jacques.berger@code3.ca>
2015-06-26 00:41:47 -0400

    Premier exemple PL/SQL

---
+++ Cours7/proc.sql
@@ -0,0 +1,6 @@
+drop procedure afficherParametre;
+
+create procedure afficherParametre
+  (param NUMBER) is
+  dbms_output.put_line('Voici le parametre : ' || to_char(param));
+end afficherParametre;
