8c4dc76949531c72b5c1ec286f0395f99d2561a1
Jacques Berger <jacques.berger@code3.ca>
2015-06-26 01:16:34 -0400

    Deuxième exemple de procédure

---
+++ Cours7/2.sql
@@ -0,0 +1,18 @@
+show errors
+
+create or replace procedure afficherNombreAlbums is
+  nombreAlbums NUMBER;
+  maxAnnee Album.annee%type;
+begin
+  select count(*)
+  into nombreAlbums
+  from album;
+
+  select max(annee)
+  into maxAnnee
+  from album;
+
+  dbms_output.put_line('Nombre albums : ' || to_char(nombreAlbums));
+  dbms_output.put_line('Max annee : ' || to_char(maxAnnee));
+end;
+/
