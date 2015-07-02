1406378a96164bce7cf19b5654de46c71a971895
Jacques Berger <jacques.berger@code3.ca>
2015-07-02 10:29:35 -0400

    Exemple de fonction

---
+++ Cours8/fonction.sql
@@ -0,0 +1,18 @@
+create or replace function obtenirAnneeDernierAlbum
+  (nomArtiste artiste.nom%type) return album.annee%type is
+  anneeMax album.annee%type;
+begin
+  select max(annee)
+  into anneeMax
+  from album
+    inner join artiste on (album.artiste_id = artiste.id)
+  where artiste.nom = nomArtiste;
+
+  return anneeMax;
+
+exception
+  when no_data_found then
+    return NULL;
+  when others then
+    raise_application_error(-20001, 'Erreur dans la fonction');
+end;
